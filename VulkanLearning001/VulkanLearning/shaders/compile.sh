00859ce01f37c34d75ac1a09cbab7757cbbf8b03
vinsentli <vinsentli@tencent.com>
2025-02-23 16:35:53 +0800

    添加一组直接画三角形的shader

--- VulkanLearning001/VulkanLearning/shaders/compile.sh
+++ VulkanLearning001/VulkanLearning/shaders/compile.sh
@@ -5,3 +5,6 @@
 /Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator particle_frag.glsl -o particle_frag.spv -V -S frag
 /Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator particle_comp.glsl -o particle_comp.spv -V -S comp
 
+/Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator triangle_vert.glsl -o triangle_vert.spv -V -S vert
+/Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator triangle_frag.glsl -o triangle_frag.spv -V -S frag
+
