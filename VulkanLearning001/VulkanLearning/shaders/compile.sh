1466c85fbf6d811ece03f0ae622687557934833c
vinsentli <vinsentli@tencent.com>
2025-02-23 16:35:53 +0800

    rename shaders

--- VulkanLearning001/VulkanLearning/shaders/compile.sh
+++ VulkanLearning001/VulkanLearning/shaders/compile.sh
@@ -1,7 +1,7 @@
 /Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator -V shader.vert
 /Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator -V shader.frag
 
-/Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator particle_vertex.glsl -o particle_vertex.spriv -V -S vert
-/Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator particle_frag.glsl -o particle_frag.spriv -V -S frag
-/Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator particle_comp.glsl -o particle_comp.spriv -V -S comp
+/Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator particle_vert.glsl -o particle_vert.spv -V -S vert
+/Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator particle_frag.glsl -o particle_frag.spv -V -S frag
+/Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator particle_comp.glsl -o particle_comp.spv -V -S comp
 
