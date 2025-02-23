c7f694ef4a3b3cc1a6654da7a2e3d84fd0d69a2b
vinsentli <vinsentli@tencent.com>
2025-02-23 16:35:53 +0800

    add particle shaders

--- VulkanLearning001/VulkanLearning/shaders/compile.sh
+++ VulkanLearning001/VulkanLearning/shaders/compile.sh
@@ -1,2 +1,7 @@
 /Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator -V shader.vert
 /Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator -V shader.frag
+
+/Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator particle_vertex.glsl -o particle_vertex.spriv -V -S vert
+/Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator particle_frag.glsl -o particle_frag.spriv -V -S frag
+/Users/lihuafeng/VulkanSDK/1.3.296.0/macOS/bin/glslangValidator particle_comp.glsl -o particle_comp.spriv -V -S comp
+
